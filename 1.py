6300c7fc554a734e1ad69b1bb27b784d1cc39e25
Abobus2283414152135125215 <alexmagikov228@gmail.com>
2023-01-23 15:12:23 +0600

    1 2

--- 1.py
+++ 1.py
@@ -1,9 +1,17 @@
+
 def ob(V0,V1,t):
     def distance(acceleration):
         def wrap():
-            a = acceleration(V0,V1,t)
-            S = V0*t+(a*(t**2)/2)
-            print(S)
+            try:
+                a = acceleration(V0,V1,t)
+                S = V0*t+(a*(t**2)/2)
+                print(S)
+            except TypeError:
+                print('НЕЛЬЗЯ ВВОДИТЬ НЕЧИСЛА')
+            except ZeroDivisionError:
+                print('T НЕ МОЖЕТ БЫТЬ 0')
+
+
         return wrap
     return distance
 @ob(10,20,30)
